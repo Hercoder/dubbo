603f66315bb5a5b3e8a9e4eb112dd461b2bd65b3
calliopsis <athk001@yeah.net>
2022-04-07 07:39:16 +0800

    初始化

--- dubbo-demo/dubbo-demo-xml/dubbo-demo-xml-consumer/src/main/java/org/apache/dubbo/demo/consumer/ApplicationConsumer.java
+++ dubbo-demo/dubbo-demo-xml/dubbo-demo-xml-consumer/src/main/java/org/apache/dubbo/demo/consumer/ApplicationConsumer.java
@@ -25,7 +25,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.concurrent.CompletableFuture;
 
-public class Application {
+public class ApplicationConsumer {
     /**
      * In order to make sure multicast registry works, need to specify '-Djava.net.preferIPv4Stack=true' before
      * launch the application
@@ -33,10 +33,10 @@ public class Application {
     public static void main(String[] args) throws Exception {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring/dubbo-consumer.xml");
         context.start();
-        DemoService demoService = context.getBean("demoService", DemoService.class);
+//        DemoService demoService = context.getBean("demoService", DemoService.class);
+//        RestDemoService restDemoService = context.getBean("restDemoService", RestDemoService.class);
+//        TripleService tripleService = context.getBean("tripleService", TripleService.class);
         GreetingService greetingService = context.getBean("greetingService", GreetingService.class);
-        RestDemoService restDemoService = context.getBean("restDemoService", RestDemoService.class);
-        TripleService tripleService = context.getBean("tripleService", TripleService.class);
 
         new Thread(() -> {
             while (true) {
@@ -53,40 +53,40 @@ public class Application {
             }
         }).start();
 
-        new Thread(() -> {
-            while (true) {
-                try {
-                    String restResult = restDemoService.sayHello("rest");
-                    System.out.println(restResult + " from separated thread.");
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                }
-            }
-        }).start();
-
-        new Thread(() -> {
-            while (true) {
-                try {
-                    String restResult = tripleService.hello();
-                    System.out.println(restResult + " from separated thread.");
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                }
-            }
-        }).start();
+//        new Thread(() -> {
+//            while (true) {
+//                try {
+//                    String restResult = restDemoService.sayHello("rest");
+//                    System.out.println(restResult + " from separated thread.");
+//                } catch (Exception e) {
+//                    e.printStackTrace();
+//                }
+//                try {
+//                    Thread.sleep(1000);
+//                } catch (InterruptedException e) {
+//                }
+//            }
+//        }).start();
+//
+//        new Thread(() -> {
+//            while (true) {
+//                try {
+//                    String restResult = tripleService.hello();
+//                    System.out.println(restResult + " from separated thread.");
+//                } catch (Exception e) {
+//                    e.printStackTrace();
+//                }
+//                try {
+//                    Thread.sleep(1000);
+//                } catch (InterruptedException e) {
+//                }
+//            }
+//        }).start();
 
         while (true) {
             try {
-                CompletableFuture<String> hello = demoService.sayHelloAsync("world");
-                System.out.println("result: " + hello.get());
+//                CompletableFuture<String> hello = demoService.sayHelloAsync("world");
+//                System.out.println("result: " + hello.get());
 
                 String greetings = greetingService.hello();
                 System.out.println("result: " + greetings);
